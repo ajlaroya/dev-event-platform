a2d4c81fa44396f05ad9c9354ac9da3873634605
Arthur Laroya <arthur.j.laroya@gmail.com>
2025-12-05 16:28:22 +1100

    Implement event details page with booking functionality, enhance event agenda and tags components, and update image source configuration

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { IEvent } from "@/database";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Home = async () => {
+  "use cache";
+
   const response = await fetch(`${BASE_URL}/api/events`);
   const { events } = await response.json();
 
